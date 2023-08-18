42ef532a0da884482e4757bb51874dbd61bb9b94
eszterberenyi <e.eszterberenyi@gmail.com>
2023-08-18 13:37:04 +0200

    add en page and its contents

--- pages/en.js
+++ pages/en.js
@@ -1,12 +1,69 @@
 import React from "react";
 import Layout from "../components/Layout";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import {enContent, imagePlaceholder} from "../utils/pageContents";
+import Image from "next/image";
+import {ItemTransition} from "../components/ItemTransition";
 
 export default function En() {
     return (
         <Layout
             title='En'
         >
-            <div>Content will come</div>
+            <Grid
+                container
+                spacing={3}
+                alignItems="center"
+                justifyContent="center"
+                padding='10px'
+            >
+                <ItemTransition>
+                    <Grid item container>
+                        <Typography variant='h5' fontWeight='bold'>{enContent.titles.about}</Typography>
+                    </Grid>
+                    <Grid item container xs={12} lg={6}>
+                        <Grid item>
+                            <Image
+                                src={enContent.images.aboutUs}
+                                alt='kép a projekttagokról'
+                                priority={true}
+                                style={{maxWidth: '100%', height: 'auto'}}
+                            >
+
+                            </Image>
+                        </Grid>
+                    </Grid>
+                    <Grid item container spacing={6} xs={12} lg={6}>
+                        <Grid item>
+                            <Typography>{enContent.aboutUs}</Typography>
+                        </Grid>
+                    </Grid>
+                </ItemTransition>
+
+                <ItemTransition>
+                    <Grid item container>
+                        <Typography variant='h5' fontWeight='bold'>{enContent.titles.neighborhood}</Typography>
+                    </Grid>
+                    <Grid item container xs={12} lg={6}>
+                        <Grid item>
+                            <Image
+                                src={imagePlaceholder.placeholder}
+                                alt='kép a szomszédsági projektről'
+                                priority={true}
+                                style={{maxWidth: '100%', height: 'auto'}}
+                            >
+                            </Image>
+                        </Grid>
+                    </Grid>
+                    <Grid item container spacing={6} xs={12} lg={6}>
+                        <Grid item>
+                            <Typography>{enContent.aboutNeighborhoodProject}</Typography>
+                        </Grid>
+                    </Grid>
+                </ItemTransition>
+
+            </Grid>
         </Layout>
     )
 }
--- utils/pageContents.js
+++ utils/pageContents.js
@@ -3,6 +3,11 @@ import combinedImg from '../public/landing/combined.jpg'
 import aboutUsImg from '../public/landing/bemutatkozo.jpg'
 import berthaLogo from '../public/landing/bertha_logo_BLACKonWHITE-01.svg'
 import theySayImg from '../public/landing/azt_beszelik.svg'
+import placeholder from '../public/image_placeholder.jpg'
+
+export const imagePlaceholder = {
+    placeholder: placeholder
+}
 
 export const homeContent = {
     aboutUs: 'Az Orczy Szomszédok a Gólya Szövetkezet és a Kazán Közösségi Ház Szomszédsági programjának a ház környezetéről szóló, helytörténeti projektje. Az itt élők történetein keresztül szeretnénk megismerni a véleményüket és tapasztalataikat a környékről, megérteni, hogy különböző társadalmi folyamatok hogyan alakítják a mindennapjaikat. Fontosnak tartjuk, hogy a kerületrészről azok az emberek beszéljenek akiknek a mindennapi tapasztalata ide kötődik, ezzel árnyalva az Orczy-negyed köztudatban élő rossz hírét. A projekt kiindulópontja egy hetvenkét interjúból álló kutatás, amit részvételi művészeti és közösségépítő programok, és egy kiállítás követ majd.',
@@ -19,4 +24,17 @@ export const homeContent = {
         combined: combinedImg
     },
     email: 'orczy.szomszedsagi@gmail.com'
+}
+
+export const enContent = {
+    aboutUs: 'Orczy Neighborhood Project is a neighborhood project using creative tools to build community, fight gentrification and research the local history of a stigmatized area in Budapest, Hungary. The project expands the work of the Community From Neighborhood Program, run by Kazán Community Center, a political and social space owned and used by more than ten different organizations in the same location. The Orczy Neighborhood Project was initiated by Csilla Hajdu, a community developer in the Community From Neighborhood Program, and Dorottya Poór, a visual artist and art educator. They work with a group of community developers, social workers, sociologists, cultural workers, and artists.',
+    aboutNeighborhoodProject: 'Coming soon!',
+    titles: {
+        about: 'ABOUT US',
+        neighborhood: 'COMMUNITY FROM NEIGHBORHOOD',
+    },
+    images: {
+        aboutUs: aboutUsImg,
+        neighborhood: '',
+    }
 }
