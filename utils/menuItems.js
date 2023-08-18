18008543474142968bcec23a0d7d73804f8cdf9f
eszterberenyi <e.eszterberenyi@gmail.com>
2023-08-18 13:37:39 +0200

    update menuItems

--- utils/menuItems.js
+++ utils/menuItems.js
@@ -2,17 +2,8 @@ export const menuItems = [
     { name: 'EN', link: 'en' },
     { name: 'RÓLUNK', link: 'about' },
     { name: 'FOGLALKOZÁSOK', link: 'workshops'},
-    { name: 'TÉRKÉP', link: 'map' },
+    { name: 'SZOMSZÉDSÁGI', link: 'neighborhood' },
     { name: 'KUTATÁS', link: 'research' },
     { name: 'KIÁLLÍTÁS', link: 'exhibition' },
     { name: 'MÉDIA', link: 'media' }
 ]
-
-// , dropdown: [
-//     {
-//         name: 'AKTUÁLIS', link: 'current'
-//     },
-//     {
-//         name: 'ARCHÍV', link: 'archive'
-//     }
-// ],
