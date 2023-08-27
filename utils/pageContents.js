c5fd0304e05103becf8432e6aca4cf8196f0dbab
eszterberenyi <e.eszterberenyi@gmail.com>
2023-08-27 17:42:06 +0200

    update page contents

--- utils/pageContents.js
+++ utils/pageContents.js
@@ -1,5 +1,4 @@
 import openingImg from '../public/landing/nyito.jpg'
-import combinedImg from '../public/landing/combined.jpg'
 import aboutUsImg from '../public/landing/bemutatkozo.jpg'
 import berthaLogo from '../public/landing/bertha_logo_BLACKonWHITE-01.svg'
 import theySayImg from '../public/landing/azt_beszelik.svg'
@@ -7,7 +6,7 @@ import placeholder from '../public/image_placeholder.jpg'
 import szonja from '../public/about/aboutUs/bemutatkozo deák.jpg'
 import balla from '../public/about/aboutUs/bemutatkozo balla.jpg'
 import csilla from '../public/about/aboutUs/csilla_bemutatkozo.jpg'
-import dotya from  '../public/about/aboutUs/Dotya.jpg'
+import dotya from 'public/about/aboutUs/Dotya.jpg'
 import anna from '../public/about/aboutUs/SeresAnna.jpg'
 import mate from '../public/about/aboutUs/bemutatkozo máté.jpg'
 import siteri from '../public/about/aboutUs/bemutatkozo siteri.jpg'
@@ -16,6 +15,15 @@ import pkLogo from '../public/about/aboutUs/pkk_logo_0.png'
 import jozsefvarosLogo from '../public/about/aboutUs/jozsefvaros_logo.png'
 import eventCover from '../public/exhibition/OSZ_esemeny_cover_23_08_182.jpg'
 import communityFromNeighborhood from '../public/szomszedsagbol_kozosseget.jpg'
+import p1 from '../public/neighborhood/aboutPics/DSC_0072.jpg'
+import p2 from '../public/neighborhood/aboutPics/DSC_0122.jpg'
+import p3 from '../public/neighborhood/aboutPics/DSC_0211.jpg'
+import p4 from '../public/neighborhood/aboutPics/IMG_6409.JPG'
+import p5 from '../public/neighborhood/aboutPics/IMG_20211114_125756.jpg'
+import p6 from '../public/neighborhood/aboutPics/m.jpg'
+import p7 from '../public/neighborhood/aboutPics/majjalis.jpg'
+import p8 from '../public/neighborhood/aboutPics/P1140610.JPG'
+import p9 from '../public/neighborhood/aboutPics/utcazene.jpg'
 
 export const imagePlaceholder = {
     placeholder: placeholder
@@ -33,20 +41,19 @@ export const homeContent = {
         aboutUs: aboutUsImg,
         bertha: berthaLogo,
         theySay: theySayImg,
-        combined: combinedImg
     },
     email: 'orczy.szomszedsagi@gmail.com',
     buttonImages: [
         {
             url: '/landing/buttonImages/aktualis_placeholder.jpg',
-            title: 'Kiállítás',
+            title: 'KIÁLLÍTÁS',
             width: '100%',
             link: '/exhibition',
             target: '_self'
         },
         {
             url: '/landing/buttonImages/aktualis_placeholder.jpg',
-            title: 'Megnyitó',
+            title: 'MEGNYITÓ',
             width: '100%',
             link: 'https://www.facebook.com/events/104168436118807',
             target: '_blank'
@@ -139,7 +146,7 @@ export const aboutContent = {
             },
         ],
         partners: {
-            text: 'Együttműködő szervezetek:',
+            text: 'EGYÜTTMŰKÖDŐ SZERVEZETEK:',
             partnerOne: {
                 name: 'Periféria központ',
                 logo: pkLogo,
@@ -161,7 +168,7 @@ export const workshopsContent = {
     buttonImages: [
         {
             url: '/landing/buttonImages/aktualis_placeholder.jpg',
-            title: 'Kiállítás',
+            title: 'KIÁLLÍTÁS',
             width: '100%',
             link: '/exhibition',
             target: '_self'
@@ -179,4 +186,127 @@ export const mediaContent = {
             target: '_self'
         },
     ]
+}
+
+export const neighborhoodContent = {
+    mainText: '2019-ben kezdtük meg munkánkat az Orczy út 46-48. szám alatti épületben, a Gólya Szövetkezetből kiindulva. Az épület azóta a Kazán Közösségi Ház nevet kapta, amiben jelenleg 11 szervezet dolgozik.\n' +
+        'Indulásunkkor az akkori alapítókkal együtt készítettünk egy 5 évre szóló stratégiai tervet a közösen megfogalmazott céljaink alapján. A szomszédsági programban a közösségi és szociális munka eszközeivel dolgozunk, amit kiegészít a Gólya Szövetkezet hosszú múltra visszatekintő sokszínű tudástára, valamint az itt dolgozók eltérő tapasztalatai különféle területeken. Munkánk célja, hogy a Kazán egy helyben beágyazott közösségi házként működjön, annak minden tulajdonságával és funkciójával együtt.\n' +
+        '\n' +
+        'A közösségi házzá válás azt jelenti számunkra, hogy a Kazánban szervezett programokon, jelenlévő és folyamatosan fejlesztett szolgáltatásainkon, különféle projektjeinken és a házban dolgozó szervezetek tevékenységein keresztül:\n' +
+        '\n' +
+        '- helyi társadalmi problémákra és szükségletekre reagálunk\n' +
+        '- különböző életkorú, érdeklődésű, társadalmi helyzetű és hovatartozású embereket kötünk össze\n' +
+        '- párbeszédet teremtünk eltérő nézetű és érdeklődésű emberek között\n' +
+        '- a szomszédságunkban élők otthonosan használhatják tereinket és alakíthatják azokat\n' +
+        '- programjaink megszólítsáják a helyi embereket, az ő szükségleteikhez alakítjuk őket\n' +
+        '- helyi problémákra, ügyekre segítünk szerveződni és megoldásokat találni\n' +
+        '- és együttműködünk helyi csoportokkal, szervezetekkel és intézményekkel\n' +
+        '\n' +
+        'Mindezt rugalmasan, a környezetükhöz alakítva és alkalmazkodva valósítjuk meg. \n' +
+        'Ennek érdekében az alábbi négy területen végezzük munkánkat:\n' +
+        '\n' +
+        'A Kazán Házban dolgozó szervezetekkel való együttműködés és tevékenységeik támogatása, közös projektek létrehozása\n' +
+        'Az épület fizikai adottságainak, kinézetének és szolgáltatásainak fejlesztése\n' +
+        'Kapcsolatépítés és közös projektek létrehozása a szomszédsággal\n' +
+        'Kapcsolatépítés és együttműködés helyi és kerületen kívüli csoportokkal, szervezetekkel és intézményekkel\n' +
+        '\n' +
+        'A szomszédsági indulása óta sok változáson ment keresztül, az alapító tagok közül ketten dolgozunk már csak a szomszédságiban, azonban a csapatunk időről időre bővül és alakul. Jelenleg (2023.08.) hatan vagyunk, azonban sokan kapcsolódnak a munkánkhoz lazábban, programok és projektek mentén a Gólya és a Kazán különféle területeiről, illetve a házon kívülről. Jelenlegi célunk, hogy tovább bővüljön csapatunk.\n' +
+        '\n' +
+        'Kéthetente tartunk megbeszélést, amikor a különféle futó tevékenységeinket nézzük át és tervezünk, a döntéseinket pedig főleg ezeken a találkozókon, közösen hozzuk (de egyébként is együtt dolgozunk a mindennapokban különféle területeken). Minden évben tartunk egy hosszabb stratégiai megbeszélést, ahol a következő évre tervezzük meg tevékenységeinket és vállaljuk el feladatainkat azokkal kapcsolatban. Munkánkat az éves és az 5 éves stratégiai terveink alapján szervezzük. A csoportunk és tevékenységeink alakulása gyakran új kérdések elé állít minket belső szervezeti működésükkel kapcsolatban, amikkel szintén főleg ezeken a találkozókon foglalkozunk.\n' +
+        '\n' +
+        'Fontos alap a stratégiai tervünk, mégis rugalmasan tudunk reagálni a környezetünkben felmerülő dolgokra és változásokra. Előfordul, hogy elindítunk egy projektet, amiről azt gondoljuk, a legmegfelelőbb az adott célunk eléréséhez, de közben kiderül, hogy nem úgy alakul, ahogy gondoltuk és megváltoztatjuk vagy újat indítunk helyette. (Pl. a 2022-ben fiatal felnőttek számára indított munka-klubunk időközben átalakult ifjúsági klubbá). Célunk továbbra is megőrizni ezt a rugalmasságra való képességet, ugyanakkor a már létező és futó tevékenységeinket megszilárdítani és azokra építkezni.\n',
+    mainImage: communityFromNeighborhood,
+    mainTitle: 'SZOMSZÉDSÁGBÓL KÖZÖSSÉGET PROGRAM',
+    about : {
+        title: 'TEVÉKENYSÉGÜNKRŐL',
+        pics: [
+            {
+                img: p1,
+                title: 'szomszédsági esemény kép',
+            },
+            {
+                img: p2,
+                title: 'szomszédsági esemény kép',
+            },
+            {
+                img: p3,
+                title: 'szomszédsági esemény kép',
+            },
+            {
+                img: p4,
+                title: 'szomszédsági esemény kép',
+            },
+            {
+                img: p5,
+                title: 'szomszédsági esemény kép',
+            },
+            {
+                img: p6,
+                title: 'szomszédsági esemény kép',
+                rows: 2,
+                cols: 2,
+            },
+            {
+                img: p7,
+                title: 'szomszédsági esemény kép',
+            },
+            {
+                img: p8,
+                title: 'szomszédsági esemény kép',
+            },
+            {
+                img: p9,
+                title: 'szomszédsági esemény kép',
+            },
+        ],
+        text: 'A szomszédsági egyik tevékenysége az épületbe és az épület környezetébe szervezni programokat, amik az itt élők igényeire, szükségleteire épülnek vagy reagálnak. Ezen kívül ez az egyik eszközünk arra, hogy ismerkedjünk az itt élőkkel és ők is megismerhessenek minket, be tudjuk vonni őket egyéb projektjeinkbe.\n' +
+            '2019-es indulásunk után a Covid járvány és a hozzá kapcsolódó szabályozások miatt majd 2 évig nem tudtunk igazán programszervezéssel foglalkozni, hiszen nem volt biztonságos. 2020-ban szerveztünk egy Utcazene felvonulást, a Gólya Futár cargo biciklijén VIII. kerületi zenészeket fuvaroztunk az Orczy és a Magdolna negyed különféle részeire, így az otthon lévők az ablakaikból, erkélyeikről hallhatták, a résztvevők pedig szabadtéren, biztonságosan tudtak találkozni barátaikkal, ismerőseikkel. Ezzel célunk az is volt, hogy helyi zenészeket támogassunk, hiszen a járvány miatt ők is elestek fő megélhetési forrásuktól. A programnak nagy sikere volt a kerületiek körében.\n' +
+            'A covid járvány utolsó hulláma után elkezdtünk rendszeres programokat szervezni. Ebből az egyik legnépszerűbb, legnagyobb szabású a már 2. alkalommal megrendezett őszi Főzőversenyünk. Ezeken az alkalmakon gázfőzőn, bográcsban a teraszon főznek csapatok szezonális ételt és a végén egy független zsűri dönti el, hogy ki viszi haza a vándorkupát. Ilyenkor családi és gyerekprogramokkal is készülünk. A főzőversenyen kerületi lakók, családok, baráti társaságok és szervezetek, csoportok vesznek részt minden évben. Nagy öröm számunkra, hogy ezen a programon minden alkalommal megvalósul a sokszínűség, otthontalan emberek, menekültek, értelmiségiek, romák és nem romák, fiatalok és idősek egyszerre vesznek részt a különféle tevékenységekben és töltenek időt egymással.\n' +
+            'Ezen kívül egyre szorosabban dolgozunk együtt a Gólya Szövetkezet rendezvény, koncertszervező munkacsoportjával és szervezünk olyan programokat, amik helyi családok számára érdekesek, fontosak lehetnek, mint a legutóbbi farsang, majális és karaoke-party.\n',
+    },
+    buttonImages: [
+        {
+            url: 'landing/nyito.jpg',
+            title: 'ÖNKÉNTESSÉG',
+            width: '100%',
+            link: '/neighborhood/volunteer',
+            target: '_self'
+        },
+        {
+            url: 'landing/nyito.jpg',
+            title: 'KERT PROJEKT',
+            width: '100%',
+            link: '/neighborhood/garden',
+            target: '_self'
+        },
+        {
+            url: 'landing/nyito.jpg',
+            title: 'TAJ PROJEKT',
+            width: '100%',
+            link: '/neighborhood/taj',
+            target: '_self'
+        },
+        {
+            url: 'landing/nyito.jpg',
+            title: 'EDUKÁCIÓ',
+            width: '100%',
+            link: '/neighborhood/education',
+            target: '_self'
+        },
+        {
+            url: 'landing/nyito.jpg',
+            title: 'IFJÚSÁGI KLUB',
+            width: '100%',
+            link: '/neighborhood/youthclub',
+            target: '_self'
+        },
+    ],
+    volunteer: {
+        title: 'ÖNKÉNTES MUNKA, TAPASZTALATCSERE ÉS TÁMOGATÁSI LEHETŐSÉGEK',
+        email: 'trk.zsofia@gmail.com',
+        phone: '+ 36 20 997 8986\n'
+
+    }
+
+
 }
