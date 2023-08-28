4bf981e35f88d29488399d61cbce12271969ecc9
eszterberenyi <e.eszterberenyi@gmail.com>
2023-08-28 15:39:20 +0200

    add meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -14,13 +14,27 @@ export default function Layout({children, title}) {
 
         <div className={montserrat.className}>
             <Head>
+                <meta charSet="UTF-8"/>
                 {/*<link rel="icon" href="/favicon.ico" />*/}
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <meta
-                    name="og:description"
-                    content="Az Orczy Szomszédok a Gólya Szövetkezet és a Kazán Közösségi Ház Szomszédsági programjának a ház környezetéről szóló, helytörténeti projektje."
+                    name="description"
+                    content="
+                    Az Orczy Szomszédok a Gólya Szövetkezet és a Kazán Közösségi Ház Szomszédsági programjának a ház környezetéről szóló, helytörténeti projektje.
+                    Az itt élők történetein keresztül szeretnénk megismerni a véleményüket és tapasztalataikat a környékről, megérteni, hogy különböző társadalmi folyamatok hogyan alakítják a mindennapjaikat.
+                    "
                 />
-                <meta name="og:title" content={title} />
+                <meta name="title" content={title} />
                 <title>{title}</title>
+                <meta property="og:url" content="https://szomszedsagbolkozosseget.hu/"/>
+                <meta property="og:image" content="/opengraphImg.jpg" />
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content={title}/>
+                <meta property="og:description" content="
+                    Az Orczy Szomszédok a Gólya Szövetkezet és a Kazán Közösségi Ház Szomszédsági programjának a ház környezetéről szóló, helytörténeti projektje.
+                    Az itt élők történetein keresztül szeretnénk megismerni a véleményüket és tapasztalataikat a környékről, megérteni, hogy különböző társadalmi folyamatok hogyan alakítják a mindennapjaikat.
+                    "/>
+                <meta name="google-site-verification" content="uRFiN9o-kHS4IhXFJq-5uXve-UdT6gXXNgtbjtyFjWs" />
             </Head>
             <div className={style.content}>
                 <ResponsiveAppBar/>
